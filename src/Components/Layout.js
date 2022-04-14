0dc14941188f8451956bad2988bdfe32da528a7b
azzy kesuma <official.azzy@gmail.com>
2022-04-14 08:27:20 +0700

    adding appbar

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,9 +1,52 @@
 import './Layout.css';
+import { makeStyles } from '@mui/styles';
+import { useHistory,useLocation } from 'react-router-dom';
 import { Drawer,Typography } from '@mui/material';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import { SubjectOutlined, AddCircleOutlined } from '@mui/icons-material';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import { Box } from '@mui/system';
+
+const useStyles = makeStyles({
+    active : {
+        background : '#f4f4f4'
+    }
+})
 
 const Layout = ({ children }) => {
+
+    const classes = useStyles();
+
+    const menuItems = [
+        {
+            text : 'My Note',
+            icon : <SubjectOutlined color="secondary" />,
+            path : '/'
+        },
+        {
+            text : 'Add Note',
+            icon : <AddCircleOutlined color="secondary" />,
+            path : '/create'
+        },
+    ];
+
+    const history = useHistory();
+    const location = useLocation();
+
     return (
         <div className='root'>
+
+            <AppBar className='appbar'>
+                <Toolbar>
+                    <Typography className='textTitle'>
+                        Welcome to Note
+                    </Typography>
+                </Toolbar>
+            </AppBar>
 
             {/* drawer */}
             <Drawer
@@ -14,11 +57,29 @@ const Layout = ({ children }) => {
                 paper : 'drawerPaper'
             }}
             >
-                <Typography variant="h5">
+                <Typography variant="h5" className='title'>
                     Notes Ninja
                 </Typography>
+
+                {/* list/links */}
+                {
+                    menuItems.map(item => (
+                        <List>
+                            <ListItem 
+                            key={item.text} 
+                            button
+                            onClick={() => history.push(item.path)}
+                            className={location.pathname == item.path ? classes.active : null}
+                            >
+                                <ListItemIcon>{item.icon}</ListItemIcon>
+                                <ListItemText primary={item.text} />
+                            </ListItem>
+                        </List>
+                    ))
+                }
             </Drawer>
             <div className="page">
+                <div className="margin"></div>
                 { children }
             </div>
         </div>
